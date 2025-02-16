3eac46ba25ea250351013e0eef64e3ac081b4226
maxime lapouge <dpower632@gmail.com>
2025-02-16 18:02:32 +0100

    update nav

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,42 +1,73 @@
-import {JSX} from "react";
-import "./Navbar.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faHome,
-    faLink,
-    faAddressCard,
-    faListUl,
-} from '@fortawesome/free-solid-svg-icons'
-import {NavLink} from "react-router-dom";
-export default function Navbar() : JSX.Element {
-    return(
-        <>
-            <div className="dock-navbar">
-                <div className="dock-item">
-                    <NavLink exact="true" to="/">
-                        <FontAwesomeIcon icon={faHome}/>
-                    </NavLink>
+import { JSX } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faLink, faAddressCard, faListUl } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import "./Navbar.scss";
 
-                </div>
-                <div className="dock-item">
-                    <NavLink exact="true" to="/links">
-                        <FontAwesomeIcon icon={faLink}/>
-                    </NavLink>
-                </div>
+const iconHoverEffect = {
+    whileHover: {
+        scale: 1.2,
+        y: -5,
+    },
+    transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 15,
+        duration: 0.3
+    }
+};
 
-                <div className="dock-item">
-                    <NavLink exact="true" to="/about">
-                        <FontAwesomeIcon icon={faAddressCard}/>
-                    </NavLink>
-                </div>
-
-                <div className="dock-item">
-                    <NavLink exact="true" to="/todo">
-                        <FontAwesomeIcon icon={faListUl}/>
-                    </NavLink>
-                </div>
-
+export default function Navbar(): JSX.Element {
+    return (
+        <div className="dock-navbar">
+            <div className="dock-item">
+                <NavLink exact="true" to="/" data-title="Home">
+                    <motion.div
+                        whileHover={iconHoverEffect.whileHover}
+                        transition={iconHoverEffect.transition}
+                        className="dock-item-content"
+                    >
+                        <FontAwesomeIcon icon={faHome} className="icon-color" />
+                    </motion.div>
+                </NavLink>
             </div>
-        </>
-    )
+
+            <div className="dock-item">
+                <NavLink exact="true" to="/links" data-title="Links">
+                    <motion.div
+                        whileHover={iconHoverEffect.whileHover}
+                        transition={iconHoverEffect.transition}
+                        className="dock-item-content"
+                    >
+                        <FontAwesomeIcon icon={faLink} className="icon-color" />
+                    </motion.div>
+                </NavLink>
+            </div>
+
+            <div className="dock-item">
+                <NavLink exact="true" to="/about" data-title="About">
+                    <motion.div
+                        whileHover={iconHoverEffect.whileHover}
+                        transition={iconHoverEffect.transition}
+                        className="dock-item-content"
+                    >
+                        <FontAwesomeIcon icon={faAddressCard} className="icon-color" />
+                    </motion.div>
+                </NavLink>
+            </div>
+
+            <div className="dock-item">
+                <NavLink exact="true" to="/todo" data-title="Todo">
+                    <motion.div
+                        whileHover={iconHoverEffect.whileHover}
+                        transition={iconHoverEffect.transition}
+                        className="dock-item-content"
+                    >
+                        <FontAwesomeIcon icon={faListUl} className="icon-color" />
+                    </motion.div>
+                </NavLink>
+            </div>
+        </div>
+    );
 }
--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,12 +1,16 @@
 import {JSX} from "react";
 import "./Home.scss"
 export default function Home() : JSX.Element   {
-    return(
-        <>
-            <div className="landing">
-
-
+    return (
+        <div className="landing-container">
+            <div className="quote-box">
+                <blockquote className="quote-text">
+                    <h1 className="quote">An aestetic TODO</h1>
+                    <p className="author">
+                        <small>By moxi</small>
+                    </p>
+                </blockquote>
             </div>
-        </>
-    )
+        </div>
+    );
 }
