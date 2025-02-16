5e11abf96b399577487bc027125967ad88a51c29
maxime lapouge <dpower632@gmail.com>
2025-02-16 18:25:02 +0100

    update nav and add todo logic

--- src/Screens/Todo/Todo.tsx
+++ src/Screens/Todo/Todo.tsx
@@ -1,9 +1,45 @@
 import "./Todo.scss"
-import {JSX} from "react";
+import {JSX, useEffect} from "react";
+import {useState} from "react";
 export default function Todo():JSX.Element {
+    const [task , setTask] = useState('')
+    const [tasks , setTasks] = useState<string[]>([])
+    useEffect(()=>{
+    const savedTask = localStorage.getItem("tasks");
+    if (savedTask){
+        setTasks(JSON.parse(savedTask))
+    }
+    },[])
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+const addTask = ()=> {
+    if(task.trim()!== ''){
+        setTasks([...tasks , task])
+        setTask('');
+    }
+}
+const deleteTask = (index: number)=>{
+    const newTasks = tasks.filter((_, i ) => i !== index);
+    setTasks(newTasks);
+    }
     return(
         <>
-        TODO
+        <div className="todo-container">
+            <div className="todo-input">
+                <input type="text" value={task} onChange={(e)=> setTask(e.target.value)} placeholder="Add new task"/>
+                <button onClick={addTask}> ADD</button>
+            </div>
+            <div className="tasks">
+                <ul>
+                    {tasks.map((task, index)=>(
+                        <li key={index}>{task}
+                            <button onClick={()=> deleteTask(index)}> delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
