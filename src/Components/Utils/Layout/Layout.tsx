3c00b5b16c00e0124e1c02302bb73098053b0fd4
maxime lapouge <dpower632@gmail.com>
2025-03-26 11:53:37 +0100

    unload localStorage on refresh or leave + put accent on name for AnimatedLanding.tsx

--- src/Components/Utils/Layout/Layout.tsx
+++ src/Components/Utils/Layout/Layout.tsx
@@ -3,12 +3,20 @@ import Navbar from "../../Navbar/Navbar.tsx";
 import Footer from "../../Footer/Footer.tsx";
 import { useMediaQuery } from "react-responsive";
 import { ThemeProvider  } from "../Provider/Theme/ThemeContext.tsx";
-import {JSX} from "react";
+import {JSX, useEffect} from "react";
 
 import MobileView from "../../../Screens/MobileView/MobileView.tsx";
 export default function Layout(): JSX.Element {
   const isDesktop = useMediaQuery({ minWidth: 800 });
-
+  useEffect(() => {
+    const clearLocalStorage = () => {
+      localStorage.clear();
+    };
+    window.addEventListener("beforeunload", clearLocalStorage);
+    return () => {
+      window.removeEventListener("beforeunload", clearLocalStorage);
+    };
+  }, []);
   return (
 
     <ThemeProvider>
