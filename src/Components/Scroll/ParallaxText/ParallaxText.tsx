03ec17bb747d4bad6f62ace4f91c9741d7ae5837
maxime lapouge <dpower632@gmail.com>
2025-04-02 10:16:29 +0200

    image slider on drag close drawer :)

--- src/Components/Scroll/ParallaxText/ParallaxText.tsx
+++ src/Components/Scroll/ParallaxText/ParallaxText.tsx
@@ -1,98 +1,97 @@
-import { JSX, useEffect, useRef, useState } from "react";
-import { useLenis } from "lenis/react";
+import {JSX, useEffect, useRef, useState} from "react";
+import {useLenis} from "lenis/react";
 
 import s from "./ParallaxText.module.scss";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import BubbleText from "../../BubbleText/BubbleText.tsx";
 import MouseScrollAnimation from "../../ScrollDownInvintation/MouseScrollAnimation.tsx";
+
 interface ParallaxTextProps {
-  topText: string[];
-  bottomText: string[];
-  centerText?: string;
-  subCenterText?: string;
-  length?: number;
-  textColor?: string;
-  bgColor?: string;
+    topText: string[];
+    bottomText: string[];
+    centerText?: string;
+    subCenterText?: string;
+    length?: number;
+    textColor?: string;
+    bgColor?: string;
 }
 
 export default function ParallaxText({
-  topText,
-  bottomText,
-  centerText = "",
-  textColor = "#000",
-  bgColor = "#fff",
-  length = 5,
-  subCenterText = "",
-}: ParallaxTextProps): JSX.Element {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  const [scrollValue, setScrollValue] = useState(0);
-  const [maxScroll, setMaxScroll] = useState(1);
-  const createRepeatingText = (textArray: string[], repeatCount: number) => {
-    const repeatedText = Array(repeatCount).fill(textArray).flat();
-    return repeatedText.map((text, index) => (
-      <span key={`repeated-text-${index}`} className={s.spanText}>
+                                         topText,
+                                         bottomText,
+                                         centerText = "",
+                                         textColor = "#000",
+                                         bgColor = "#fff",
+                                         length = 5,
+                                     }: ParallaxTextProps): JSX.Element {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const [scrollValue, setScrollValue] = useState(0);
+    const [maxScroll, setMaxScroll] = useState(1);
+    const createRepeatingText = (textArray: string[], repeatCount: number) => {
+        const repeatedText = Array(repeatCount).fill(textArray).flat();
+        return repeatedText.map((text, index) => (
+            <span key={`repeated-text-${index}`} className={s.spanText}>
         {text}
       </span>
-    ));
-  };
-  useLenis(({ scroll }) => {
-    if (containerRef.current) {
-      const rect = containerRef.current.getBoundingClientRect();
-      const scrollHeight = containerRef.current.clientHeight;
-      const maxScrollInside = scrollHeight - window.innerHeight;
-      if (rect.top < 0 && rect.bottom > 0) {
-        const normalizedScroll = Math.max(
-          0,
-          Math.min(-rect.top / maxScrollInside, 1)
+        ));
+    };
+    useLenis(({scroll}) => {
+        if (containerRef.current) {
+            const rect = containerRef.current.getBoundingClientRect();
+            const scrollHeight = containerRef.current.clientHeight;
+            const maxScrollInside = scrollHeight - window.innerHeight;
+            if (rect.top < 0 && rect.bottom > 0) {
+                const normalizedScroll = Math.max(
+                    0,
+                    Math.min(-rect.top / maxScrollInside, 1)
+                );
+                setScrollValue(normalizedScroll);
+            }
+        }
+    });
+
+    useEffect(() => {
+        setMaxScroll(
+            scrollContainerRef.current.scrollWidth - containerRef.current.clientWidth
         );
-        setScrollValue(normalizedScroll);
-      }
-    }
-  });
+    }, []);
 
-  useEffect(() => {
-    setMaxScroll(
-      scrollContainerRef.current.scrollWidth - containerRef.current.clientWidth
+    return (
+        <section
+            ref={containerRef}
+            className={s.parallaxTextContainer}
+            style={
+                {
+                    backgroundColor: bgColor,
+                    "--length": length?.toString(),
+                    "--text-color": textColor,
+                } as React.CSSProperties
+            }
+        >
+            <div className={s.stickyContent} ref={scrollContainerRef}>
+                <motion.div
+                    style={{x: `-${(scrollValue * maxScroll * 0.5) % maxScroll}px`}}
+                    className={s.parallaxTextTop}
+                >
+                    {createRepeatingText(topText, 3)}
+                </motion.div>
+                <div className={s.centerTextContainer}>
+                    {centerText && <h1 className={s.centerTitle}>
+                      <BubbleText text={centerText} color="#efe07d"/>
+                    </h1>}
+
+                    <MouseScrollAnimation/>
+                </div>
+
+
+                <motion.div
+                    style={{x: `${(scrollValue * maxScroll * 0.5) % maxScroll}px`}}
+                    className={s.parallaxTextBottom}
+                >
+                    {createRepeatingText(bottomText, 3)}
+                </motion.div>
+            </div>
+        </section>
     );
-  }, []);
-
-  return (
-    <section
-      ref={containerRef}
-      className={s.parallaxTextContainer}
-      style={
-        {
-          backgroundColor: bgColor,
-          "--length": length?.toString(),
-          "--text-color": textColor,
-        } as React.CSSProperties
-      }
-    >
-      <div className={s.stickyContent} ref={scrollContainerRef}>
-        <motion.div
-          style={{ x: `-${(scrollValue * maxScroll * 0.5) % maxScroll}px` }}
-          className={s.parallaxTextTop}
-        >
-          {createRepeatingText(topText, 3)}
-        </motion.div>
-        <div className={s.centerTextContainer}>
-          {centerText && <h1 className={s.centerTitle}>
-            <BubbleText text={centerText} color="#efe07d"/>
-          </h1>}
-
-          <MouseScrollAnimation/>
-        </div>
-
-
-
-        <motion.div
-          style={{ x: `${(scrollValue * maxScroll * 0.5) % maxScroll}px` }}
-          className={s.parallaxTextBottom}
-        >
-          {createRepeatingText(bottomText, 3)}
-        </motion.div>
-      </div>
-    </section>
-  );
 }
--- src/Screens/MobileView/MobileView.tsx
+++ src/Screens/MobileView/MobileView.tsx
@@ -2,199 +2,202 @@ import s from "./MobileView.module.scss";
 import React, {JSX, useState, useEffect, lazy, useRef} from "react";
 
 const ZoomText = lazy(() => import("../../Components/Scroll/Zoom/Zoom.tsx"));
-const ScrollProgress = lazy( () => import ("../../Components/Scroll/ScrollProgress/ScrollProgress.tsx"));
-const DragCloseDrawer = lazy(()=>import("../../Components/Modal/DragCloseDrawer/DragCloseDrawer.tsx"));
-const  SharedLayout = lazy(()=> import("../../Components/Utils/SharedLayout/SharedLayout.tsx"));
-const PokemonLikeCarousel = lazy(()=>import("../../Components/Carrousel/PokemonLikeCarrousel/PokemonCarousel.tsx"))
+const ScrollProgress = lazy(() => import ("../../Components/Scroll/ScrollProgress/ScrollProgress.tsx"));
+const DragCloseDrawer = lazy(() => import("../../Components/Modal/DragCloseDrawer/DragCloseDrawer.tsx"));
+const SharedLayout = lazy(() => import("../../Components/Utils/SharedLayout/SharedLayout.tsx"));
+const SlideInfiniteCarousel = lazy(() => import("../../Components/Carrousel/SlideInfiniteCarousel/SlideInfiniteCarousel.tsx"))
 import {SendEmail} from "../../Components/Utils/SendEmail/SendEmail.ts";
-import { Project } from "../../Components/Utils/Types/ProjectType.ts";
-import { getProjects } from "../../Components/Project/InitProject.ts";
+import {Project} from "../../Components/Utils/Types/ProjectType.ts";
+import {getProjects} from "../../Components/Project/InitProject.ts";
 import links from "../../Components/Utils/_const/_links.ts";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faServer, faNetworkWired , faFileLines } from "@fortawesome/free-solid-svg-icons";
-import { faJs ,faLinkedin , faGithub} from "@fortawesome/free-brands-svg-icons";
-import LoremIpsum from "react-lorem-ipsum";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faServer, faNetworkWired, faFileLines} from "@fortawesome/free-solid-svg-icons";
+import {faJs, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+
 import cn from "clsx";
 
 
-
-
 export default function MobileView(): JSX.Element {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [dragCloseDrawerOpen , setDragCloseDrawerOpen ] = useState(false);
-  const [openedProject , setOpenedProject] = useState(null);
-const emailRef = useRef<HTMLFormElement>(null)
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const data: any = await getProjects();
-      setProjects(data);
-    };
-    fetchProjects();
-  }, []);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [dragCloseDrawerOpen, setDragCloseDrawerOpen] = useState(false);
+    const [openedProject, setOpenedProject] = useState(null);
+    const emailRef = useRef<HTMLFormElement>(null)
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const data: any = await getProjects();
+            setProjects(data);
+        };
+        fetchProjects();
+    }, []);
+
     function getFormattedUrl(url: string) {
         return url.startsWith("http://") || url.startsWith("https://")
             ? url
             : `https://${url}`;
     }
-  return (
-      <div className={s.MobileView}>
-          <ScrollProgress/>
-          <ZoomText
-              title1={"Portfolio"}
-              title2={"Maxime Lapouge"}
-              text={`Étudiant\nDéveloppeur\nIPI ${new Date().getFullYear()}`}
-          />
-          <section className={s.MobilePresentation}>
-              <h2>Passioné depuis le plus jeune age</h2>
-              <div className="text-container">
-                  <p>
-                      A 5 ans la premiere manette de NES dans les mains , 8 ans plus tard je decouvre la programmation
-                      avec
-                      python .
-                  </p>
-                  <p>Malheuresement en manque d'idée il aura fallu attendre encore quasiment 10 ans pour que je me
-                      relance
-                      dans l'aventure du développement</p>
-              </div>
-              <h2>Ce que je souhaite faire dans le futur : </h2>
-              <div className="text-container">
-                  <p>Un de mes objectifs principaux étant de me retrouver dans 5 ans dans les presentation des
-                      Awwwards</p>
-                  <p>Ou du moins pouvoir experimenté pleinement de creative web / WEBGL</p>
-              </div>
-              <h2>Comment je compte y parvenir : </h2>
-              <div className="text-container">
-                  <p>En recherchant des experience professionlles enrichissantes</p>
-                  <p>En suivant la formation de ThreeJS journey</p>
 
-              </div>
-              <h3 className="subPres">Sans oublier de m'autoformer sur :</h3>
-              <div className={s.skillsContainer}>
-                  {[
-                      {
-                          icon: faNetworkWired,
-                          title: "Réseaux",
-                          description: [
-                              "Création d'un serveur WEB en cour",
-                              "Git",
-                              "CI / CD",
-                          ],
-                      },
-                      {
-                          icon: faJs,
-                          title: "Frontend",
-                          description: ["Angular ", "React", "TypeScript"],
-                      },
-                      {
-                          icon: faServer,
-                          title: "Backend",
-                          description: ["API REST CRUD", "Java Spring boot ", "Python"],
-                      },
-                  ].map((skill, index) => (
-                      <div
-                          key={skill.title}
-                          className={`${s.skillRow} ${index % 2 === 0 ? s.left : s.right}`}
-                      >
-                          <div className={s.skillText}>
-                              <h3>{skill.title}</h3>
-                              {skill.description.map((line, index) => (
-                                  <p key={index}>{line}</p>
-                              ))}
-                          </div>
-                          <div className={s.card}>
-                              <div className={s.cardHeader}>
-                                  <FontAwesomeIcon icon={skill.icon}/>
-                              </div>
-                          </div>
-                      </div>
-                  ))}
-              </div>
-          </section>
-          <section className={s.MobileProject}>
-              <h2>Mes réalisations:</h2>
-              <SharedLayout projects={projects} setOpen={setDragCloseDrawerOpen} setOpenedProject={setOpenedProject} />
-              {dragCloseDrawerOpen && openedProject && (
-                  <DragCloseDrawer isOpen={dragCloseDrawerOpen} setIsOpen={setDragCloseDrawerOpen}>
-                      <div className={s.ProjectPresContainer}>
-                          <div className={s.ProjectPres}>
-                              {openedProject.presentation?.map((item, index) => {
-                                  if (typeof item === "string") {
-                                      return <p key={index} className="project-pres-content">{item}</p>;
-                                  } else if (item.title) {
-                                      return <h3 key={index} className="project-pres-title">{item.title}</h3>;
-                                  } else if (item.list) {
-                                      return (
-                                          <ul key={index} className="project-pres-list">
-                                          {item.list.map((listItem, i) => (
-                                                  <li key={i}>{listItem}</li>
-                                              ))}
-                                          </ul>
-                                      );
-                                  }
-                                  return null;
-                              })}
-                          </div>
+    return (
+        <div className={s.MobileView}>
+            <ScrollProgress/>
+            <ZoomText
+                title1={"Portfolio"}
+                title2={"Maxime Lapouge"}
+                text={`Étudiant\nDéveloppeur\nIPI ${new Date().getFullYear()}`}
+            />
+            <section className={s.MobilePresentation}>
+                <h2>Passioné depuis le plus jeune age</h2>
+                <div className="text-container">
+                    <p>
+                        A 5 ans la premiere manette de NES dans les mains , 8 ans plus tard je decouvre la programmation
+                        avec
+                        python .
+                    </p>
+                    <p>Malheuresement en manque d'idée il aura fallu attendre encore quasiment 10 ans pour que je me
+                        relance
+                        dans l'aventure du développement</p>
+                </div>
+                <h2>Ce que je souhaite faire dans le futur : </h2>
+                <div className="text-container">
+                    <p>Un de mes objectifs principaux étant de me retrouver dans 5 ans dans les presentation des
+                        Awwwards</p>
+                    <p>Ou du moins pouvoir experimenté pleinement de creative web / WEBGL</p>
+                </div>
+                <h2>Comment je compte y parvenir : </h2>
+                <div className="text-container">
+                    <p>En recherchant des experience professionlles enrichissantes</p>
+                    <p>En suivant la formation de ThreeJS journey</p>
+
+                </div>
+                <h3 className="subPres">Sans oublier de m'autoformer sur :</h3>
+                <div className={s.skillsContainer}>
+                    {[
+                        {
+                            icon: faNetworkWired,
+                            title: "Réseaux",
+                            description: [
+                                "Création d'un serveur WEB en cour",
+                                "Git",
+                                "CI / CD",
+                            ],
+                        },
+                        {
+                            icon: faJs,
+                            title: "Frontend",
+                            description: ["Angular ", "React", "TypeScript"],
+                        },
+                        {
+                            icon: faServer,
+                            title: "Backend",
+                            description: ["API REST CRUD", "Java Spring boot ", "Python"],
+                        },
+                    ].map((skill, index) => (
+                        <div
+                            key={skill.title}
+                            className={`${s.skillRow} ${index % 2 === 0 ? s.left : s.right}`}
+                        >
+                            <div className={s.skillText}>
+                                <h3>{skill.title}</h3>
+                                {skill.description.map((line, index) => (
+                                    <p key={index}>{line}</p>
+                                ))}
+                            </div>
+                            <div className={s.card}>
+                                <div className={s.cardHeader}>
+                                    <FontAwesomeIcon icon={skill.icon}/>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <section className={s.MobileProject}>
+                <h2>Mes réalisations:</h2>
+                <SharedLayout projects={projects} setOpen={setDragCloseDrawerOpen} setOpenedProject={setOpenedProject}/>
+                {dragCloseDrawerOpen && openedProject && (
+                    <DragCloseDrawer isOpen={dragCloseDrawerOpen} setIsOpen={setDragCloseDrawerOpen}>
+                        <div className={s.ProjectPresContainer}>
+                            <div className={s.ProjectPres}>
+                                {openedProject.presentation?.map((item, index) => {
+                                    if (typeof item === "string") {
+                                        return <p key={index} className="project-pres-content">{item}</p>;
+                                    } else if (item.title) {
+                                        return <h3 key={index} className="project-pres-title">{item.title}</h3>;
+                                    } else if (item.list) {
+                                        return (
+                                            <ul key={index} className="project-pres-list">
+                                                {item.list.map((listItem, i) => (
+                                                    <li key={i}>{listItem}</li>
+                                                ))}
+                                            </ul>
+                                        );
+                                    }
+                                    return null;
+                                })}
+                            </div>
 
 
-                          <div className={s.ProjectLinksContainer}>
-                              {openedProject.links && openedProject.links.map((link, index) => (
-                                  <div key={index} className={s.ProjectLink}>
-                                      <a href={getFormattedUrl(link.url)}>{link.name}</a>
+                            <div className={s.ProjectLinksContainer}>
+                                {openedProject.links && openedProject.links.map((link, index) => (
+                                    <div key={index} className={s.ProjectLink}>
+                                        <a href={getFormattedUrl(link.url)}>{link.name}</a>
 
-                                  </div>
-                              ))}
-                          </div>
+                                    </div>
+                                ))}
+                            </div>
 
-                          <div className={s.ProjecImgContainer}>
-                              {openedProject.img && <PokemonLikeCarousel images={openedProject.img} />}
-                          </div>
+                            <div className={s.ProjecImgContainer}>
+                                {openedProject.img && <SlideInfiniteCarousel images={openedProject.img}/>}
+                            </div>
 
-                      </div>
-                  </DragCloseDrawer>
-              )}
-          </section>
-          <section className={s.MobileContact}>
-              <h2>Me contacter : </h2>
-              <div className={s.Mobileform}>
-                  <div className={s.ContactLinksContainer}>
-                      <a href={links.externalLinks.linkedin}>
-                          <div className={s.contactItem}>
-                              <p>LinkedIn</p>
-                              <div className={s.contactLink}>
-                                  <FontAwesomeIcon icon={faLinkedin}/>
-                              </div>
-                          </div>
-                      </a>
+                        </div>
+                    </DragCloseDrawer>
+                )}
+            </section>
+            <section className={s.MobileContact}>
+                <h2>Me contacter : </h2>
+                <div className={s.Mobileform}>
+                    <div className={s.ContactLinksContainer}>
+                        <a href={links.externalLinks.linkedin}>
+                            <div className={s.contactItem}>
+                                <p>LinkedIn</p>
+                                <div className={s.contactLink}>
+                                    <FontAwesomeIcon icon={faLinkedin}/>
+                                </div>
+                            </div>
+                        </a>
 
-                      <a href={links.externalLinks.github}>
-                          <div className={s.contactItem}>
-                              <p>Github </p>
-                              <div className={s.contactLink}>
-                                  <FontAwesomeIcon icon={faGithub}/>
-                              </div>
-                          </div>
-                      </a>
-                      <a href={links.downloadLink.cv} download="Maxime Lapouge CV" >
-                          <div className={s.contactItem}>
-                              <p>Voir le CV  </p>
-                              <div className={s.contactLink}>
-                                  <FontAwesomeIcon icon={faFileLines}/>
-                              </div>
-                          </div>
-                      </a>
-                  </div>
-                  <div className={s.contactFormContainer}>
-                      <form ref={emailRef} onSubmit={(e) => SendEmail(e, emailRef)} className={s.ContactForm}>
-                          <input type="email" name="user_email" placeholder="Votre Email" className={cn(s.ContactInput , "email")}/>
-                          <input type="text" name="user_subject" placeholder="Sujet" className={cn(s.ContactInput , "subject")}/>
-                          <textarea name="user_message" placeholder="Votre Message" className={cn(s.ContactInput , "message")}></textarea>
-                          <button type="submit" className={s.ContactButton}>Envoyer</button>
-                      </form>
-                  </div>
+                        <a href={links.externalLinks.github}>
+                            <div className={s.contactItem}>
+                                <p>Github </p>
+                                <div className={s.contactLink}>
+                                    <FontAwesomeIcon icon={faGithub}/>
+                                </div>
+                            </div>
+                        </a>
+                        <a href={links.downloadLink.cv} download="Maxime Lapouge CV">
+                            <div className={s.contactItem}>
+                                <p>Voir le CV </p>
+                                <div className={s.contactLink}>
+                                    <FontAwesomeIcon icon={faFileLines}/>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className={s.contactFormContainer}>
+                        <form ref={emailRef} onSubmit={(e) => SendEmail(e, emailRef)} className={s.ContactForm}>
+                            <input type="email" name="user_email" placeholder="Votre Email"
+                                   className={cn(s.ContactInput, "email")}/>
+                            <input type="text" name="user_subject" placeholder="Sujet"
+                                   className={cn(s.ContactInput, "subject")}/>
+                            <textarea name="user_message" placeholder="Votre Message"
+                                      className={cn(s.ContactInput, "message")}></textarea>
+                            <button type="submit" className={s.ContactButton}>Envoyer</button>
+                        </form>
+                    </div>
 
-              </div>
-          </section>
-      </div>
-  )
-      ;
+                </div>
+            </section>
+        </div>
+    )
+        ;
 }
